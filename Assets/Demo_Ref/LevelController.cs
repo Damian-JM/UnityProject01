e2db782de050050b1f4dd66738d40a67de40993f
Damian <menera.damian@gmail.com>
2020-08-31 16:17:41 -0500

    First

---
+++ Assets/Demo_Ref/LevelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] Light _lightObject = null;
+    [SerializeField] float _rangeIncrement = .2f;
+    [SerializeField] SceneLoader _sceneLoader = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _sceneLoader.ReloadLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RandomizeColor();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            IncreaseRange(_rangeIncrement);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DecreaseRange(_rangeIncrement);
+        }
+    }
+
+    void IncreaseRange(float increaseAmount)
+    {
+        _lightObject.range += increaseAmount;
+    }
+
+    void DecreaseRange(float decreaseAmount)
+    {
+        _lightObject.range -= decreaseAmount;
+    }
+
+    void RandomizeColor()
+    {
+        // decide random color values (R,G,B). Use f for float
+        // Color class takes 0-1, NOT 0-255
+        float randomR = UnityEngine.Random.Range(0, 1f);
+        float randomG = UnityEngine.Random.Range(0, 1f);
+        float randomB = UnityEngine.Random.Range(0, 1f);
+        // alpha 0-1 is how opaque. Leave at 1 for no transparency
+        // create the new color based on values
+        Color newColor = new Color(randomR, randomG, randomB, 1);
+        Debug.Log(newColor);
+        // change the light color
+        _lightObject.color = newColor;
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 2f;
+
+    void Start()
+    {
+
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
