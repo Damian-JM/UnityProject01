64e6ed55cd3276dac725b215528266b9802416e0
Damian <menera.damian@gmail.com>
2020-08-31 20:51:12 -0500

    Second Version

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    [SerializeField] Transform _objectToFollow; 
+    Vector3 _objectOffset;
+
+    private void Awake()
+    {
+        _objectOffset = this.transform.position - _objectToFollow.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = _objectToFollow.position + _objectOffset;
+    }
+}
---
+++ Assets/Scripts/playerShip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class playerShip : MonoBehaviour
+
+
+{
+    [SerializeField] float _moveSpeed = 12f;
+    [SerializeField] float _turnSpeed = 3f;
+
+    Rigidbody _rb = null;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveShip();
+        turnShip();
+    }
+
+    void moveShip()
+    {
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * _moveSpeed;
+
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+
+        _rb.AddForce(moveDirection);
+    }
+
+    void turnShip()
+    {
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * _turnSpeed;
+
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+
+        _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+
+    public void Kill()
+    {
+        UnityEngine.Debug.Log("Player has been killed");
+        this.gameObject.SetActive(false);
+    }
+}
